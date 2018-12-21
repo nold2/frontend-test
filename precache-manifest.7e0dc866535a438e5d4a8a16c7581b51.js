d561ad2b3af21aec8a205813faf3864369927bf3
Arnold Sebastian <arnold_sebastian_16@hotmail.com>
2018-12-21 22:15:47 +0700

    Updates

---
+++ precache-manifest.7e0dc866535a438e5d4a8a16c7581b51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "62f2f18198e5e5318e50",
+    "url": "/dana-cita-front-end-test/static/js/runtime~main.62f2f181.js"
+  },
+  {
+    "revision": "6b624c7e0a7f57d31f91",
+    "url": "/dana-cita-front-end-test/static/js/main.6b624c7e.chunk.js"
+  },
+  {
+    "revision": "988e93a5fe8afd9a5b13",
+    "url": "/dana-cita-front-end-test/static/js/3.988e93a5.chunk.js"
+  },
+  {
+    "revision": "b56a509785e04b74e252",
+    "url": "/dana-cita-front-end-test/static/js/2.b56a5097.chunk.js"
+  },
+  {
+    "revision": "5c9ff1365c753a345ba4",
+    "url": "/dana-cita-front-end-test/static/js/1.5c9ff136.chunk.js"
+  },
+  {
+    "revision": "8ca468074dec99d9c60b91a8838f9b61",
+    "url": "/dana-cita-front-end-test/index.html"
+  }
+];
